24d73dcf103013117c5273ff79e10e88ab5ff7fa
jorge13mtb <jorge13mtb@gmail.com>
2014-04-08 21:14:17 -0600

    Api controlador - Autenticar usuario

--- app/controllers/api/centros_controller.rb
+++ app/controllers/api/centros_controller.rb
@@ -2,4 +2,23 @@ class Api::CentrosController < ApplicationController
   def index
     @centros = Centro.all
   end
+
+  def autenticar_usuario
+   @mensaje = Mensaje.new
+
+    usuario = Usuario.find_by Email: params[:email]
+    if usuario && usuario.authenticate(params[:password])
+      @mensaje.respuesta = "Si"
+    else
+      @mensaje.respuesta = "No"
+   end
+  end
+end
+
+
+# Esta clase es para devolver el token
+# Estoy usando la geme jsonBuilder
+class Mensaje
+  attr_accessor :respuesta
+  @respuesta
 end
