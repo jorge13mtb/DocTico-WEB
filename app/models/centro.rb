1fa06022540f7b1cef465428bf2a6454875787c7
jorge13mtb <jorge13mtb@gmail.com>
2014-04-12 18:27:58 -0600

    Quitar validacion del tamaño del nombre del centro, y agregar validacion de presencia para todos los datos del centro de salud

--- app/models/centro.rb
+++ app/models/centro.rb
@@ -1,6 +1,10 @@
 class Centro < ActiveRecord::Base
   belongs_to :usuario
   validates :usuario_id, presence: true
-  validates :nombre, presence: true,
-                    length: { minimum: 5 }
+  validates :nombre, presence: true
+  validates :latitud, presence: true
+  validates :longitud, presence: true
+  validates :tipo, presence: true
+  validates :horario, presence: true
+  validates :descripcion, presence: true
 end
